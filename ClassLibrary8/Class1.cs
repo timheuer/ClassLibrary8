620bb6fe19930d1b65d921c7a5ccc92ed013d9cf
Tim Heuer <tim@timheuer.com>
2022-02-12 14:20:07 -0800

    Add project files.

---
+++ ClassLibrary8/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary8;
+
+public class Class1
+{
+    public int Add(int firstNumber, int secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+using ClassLibrary8;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var c = new Class1();
+        Assert.AreEqual(3, c.Add(1, 2));
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        var c = new Class1();
+        Assert.IsFalse(7 == c.Add(1, 2));
+    }
+}
